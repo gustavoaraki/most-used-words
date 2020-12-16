793a182e8b3adc04bea9699443af52708a0ed2af
Luis Gustavo Araki <luisaraki@hotmail.com>
2020-12-16 00:22:52 -0300

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-container fluid>
+        <v-form>
+          <v-file-input 
+            label="Selecione as legendas..."
+            prepend-icon="mdi-message-text"
+            append-outer-icon="mdi-send"
+            outlined
+            multiple 
+            chips 
+            v-model="files"
+            @click:append-outer="processSubtitles" />
+        </v-form>
+        <div class="pills">
+            <Pill v-for="word in groupedWords" :key="word.name" 
+                :name="word.name" :amount="word.amount" />
+        </div> 
+    </v-container>
+</template>
+
+<script>
+import { ipcRenderer } from 'electron'
+import Pill from "./Pill"
+
+export default {
+    components: { Pill },
+  data: function() {
+    return {
+      files: [],
+      groupedWords: [],
+    };
+  },
+  methods: { 
+    processSubtitles(){
+      console.log(this.files)
+      const paths = this.files.map(f => f.path)
+      ipcRenderer.send('process-subtitles', paths)
+      ipcRenderer.on('process-subtitles', (event, resp) => {
+        this.groupedWords = resp
+      })
+    }
+  }
+};
+</script>
+
+<style>
+
+  .pills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true
+    }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
